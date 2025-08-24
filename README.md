03f4e05c19dcdbb6d3c65c81a934dbcae3288bfa
Ngoc Marcińczyk <98778057+Ngoctranpl@users.noreply.github.com>
2025-08-24 12:43:56 +0000

    updating tracker

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 - [ ] get the price of the dress from this URL [dress from Mohito](https://www.mohito.com/pl/pl/sukienka-maxi-z-wiskozy-2-668fv-08p)
 - [ ] save the price in a csv file
 - [ ] send elert to email when the price drop
+- [ ] write unit test to test the code
+- [ ] set the program to automatically run once a day at 9am 
--- tracker.py
+++ tracker.py
@@ -3,9 +3,11 @@ from bs4 import BeautifulSoup
 import json
 from datetime import date
 import csv
+import sys
 
 
 URL = "https://www.mohito.com/pl/pl/sukienka-maxi-z-wiskozy-2-668fv-08p"
+TARGET_PRICE = 249.99
 
 
 def main():
@@ -13,24 +15,34 @@ def main():
     today = date.today().strftime("%d/%m/%y")
     print(f"Price on {today} is {price} {currency}")
     save(today, price, currency)
-    print("Price is logged into price_report.csv file")
+    if price < TARGET_PRICE:
+        sent_alert(price)
 
 
+# to extract price from given website
 def get_price():
-    r = requests.get(URL)
-    soup = BeautifulSoup(r.text, features="html.parser")
-    script_tag = soup.find(type="application/ld+json")
-    content = json.loads(script_tag.string)
-    price = float(content["offers"]["price"])
-    currency = content["offers"]["priceCurrency"]
+    try:
+        r = requests.get(URL)
+        soup = BeautifulSoup(r.text, features="html.parser")
+        script_tag = soup.find(type="application/ld+json")
+        content = json.loads(script_tag.string)
+        price = float(content["offers"]["price"])
 
-    return price, currency
+        if price:
+            currency = content["offers"]["priceCurrency"]
+
+            return price, currency
+
+    except Exception as e:
+        sys.exit(f"An error occured: {e}")
 
 
+# to log the price in a CSV file
 def save(x, y, z):
     with open("price_report.csv", mode="a", newline="") as file:
         writer = csv.writer(file)
         writer.writerow([x, y, z])
+        print("Price is logged into price_report.csv file")
 
 
 if __name__ == "__main__":
